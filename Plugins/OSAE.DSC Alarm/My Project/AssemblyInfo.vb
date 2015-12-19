e65c7514a6a72a1fb9e23596cbc29a5de77743f0
Automate1 <jr@flagg.us>
2015-12-19 14:46:45 -0500

    Update DSC Alarm AssemblyInfo.vb

--- Plugins/OSAE.DSC Alarm/My Project/AssemblyInfo.vb
+++ Plugins/OSAE.DSC Alarm/My Project/AssemblyInfo.vb
@@ -1,4 +1,4 @@
-﻿Imports System
+Imports System
 Imports System.Reflection
 Imports System.Runtime.InteropServices
 
@@ -12,7 +12,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("")> 
 <Assembly: AssemblyCompany("Automate")> 
 <Assembly: AssemblyProduct("DSC Alarm")> 
-<Assembly: AssemblyCopyright("Copyright © Automate 2011")> 
+<Assembly: AssemblyCopyright("Copyright © Automate 2014")> 
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.1.4.0")> 
-<Assembly: AssemblyFileVersion("0.1.4.0")> 
+<Assembly: AssemblyVersion("0.4.1.0")> 
+<Assembly: AssemblyFileVersion("0.4.1.0")> 
